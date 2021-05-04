224779d79dcbf2c027db5eba6af968c5b5942b67
R. Kukuh <rkukuh@gmail.com>
2021-05-04 21:10:08 +0700

    Initial Commit

---
+++ Shared/Multi_Platform_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_Platform_01App.swift
+//  Shared
+//
+//  Created by R. Kukuh on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Multi_Platform_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
