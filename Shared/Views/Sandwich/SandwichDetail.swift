ecafb079cba7bfa52e26eb97b70bc5fda5168954
R. Kukuh <rkukuh@gmail.com>
2021-05-05 16:09:11 +0700

    WIP

--- Shared/Views/Sandwich/SandwichDetail.swift
+++ Shared/Views/Sandwich/SandwichDetail.swift
@@ -13,24 +13,47 @@ struct SandwichDetail: View {
     @State private var zoomed = false
     
     var body: some View {
-        Image(sandwich.imageName)
-            .resizable()
-            .aspectRatio(contentMode: zoomed ? .fill : .fit)
-            .onTapGesture {
-                withAnimation {
-                    zoomed.toggle()
-                }
-            }
-            .edgesIgnoringSafeArea(.bottom)
+        VStack {
+            Spacer()
             
-            .navigationTitle(sandwich.name)
+            Image(sandwich.imageName)
+                .resizable()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .onTapGesture {
+                    withAnimation {
+                        zoomed.toggle()
+                    }
+                }
+            
+            Spacer()
+            
+            if sandwich.isSpicy && !zoomed {
+                HStack {
+                    Spacer()
+                    Label("Spicy", systemImage: "flame.fill")
+                    Spacer()
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .font(.headline)
+                .background(Color.red)
+                .foregroundColor(.yellow)
+                .transition(.move(edge: .trailing))
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .navigationTitle(sandwich.name)
     }
 }
 
 struct SandwichDetail_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            SandwichDetail(sandwich: sandwichData[2])
+        Group {
+            NavigationView {
+                SandwichDetail(sandwich: sandwichData[1]) // spicy
+            }
+            NavigationView {
+                SandwichDetail(sandwich: sandwichData[2]) // not spicy
+            }
         }
     }
 }
